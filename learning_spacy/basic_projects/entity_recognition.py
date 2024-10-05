b9d5f3d9669a62b8c03bc44117685433363fa80f
Efe Çelik <efecelik576@gmail.com>
2024-10-05 21:34:58 +0300

    Update entity_recognition.py

--- learning_spacy/basic_projects/entity_recognition.py
+++ learning_spacy/basic_projects/entity_recognition.py
@@ -6,7 +6,7 @@ import os, ssl
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
-#
+###
 # Load the SpaCy model
 nlp = spacy.load("en_core_web_sm")
 
