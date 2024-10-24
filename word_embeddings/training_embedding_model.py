d9c34f3c602aaf265d40c3e5251b7ba39fdd145c
Efe Çelik <efecelik576@gmail.com>
2024-10-24 15:33:27 +0300

    Update training_embedding_model.py
    I created this file for training embedding model. Didn't complete yet.

--- word_embeddings/training_embedding_model.py
+++ word_embeddings/training_embedding_model.py
@@ -1,5 +1,8 @@
+#This file didn't complete yet.
+
 from gensim.test.utils import datapath
 from gensim import utils
+import gensim.models
 
 class MyCorpus:
 
@@ -7,3 +10,11 @@ class MyCorpus:
         corpus_path = datapath('lee_background.cor')
         for line in open(corpus_path):
             yield utils.simple_preprocess(line)
+
+
+sentences = MyCorpus()
+model = gensim.models.Word2Vec(sentences=sentences)
+
+vec_same = model.wv['same']
+print(vec_same[:10])
+
